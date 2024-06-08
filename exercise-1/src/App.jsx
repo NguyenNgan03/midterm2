31dd07c741c748ea76cc15103b2af9b5e8209272
NguyenNgan03 <simple.270123@gmail.com>
2024-06-08 11:55:28 +0700

    bai1

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,28 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
 function App() {
-  /* You will need to use a boolean state to manage the weather */
+  const [weather, setWeather] = useState(true);
 
   function onSunClick() {
-    // Complete this code when we click on Sunny Time
+    setWeather(true);
   }
 
   function onRainClick() {
-    // Complete this code when we click on Sunny Time
+    setWeather(false);
   }
 
   function getTitle() {
-    // This function manage the H1 text, depending on the weather
+    return weather ? "It's Sunny" : "It's Rainy";
   }
 
   function getBackgroundColor() {
-    // This function manage the main class value, depending on the weather
+    return weather ? "sunny" : "rainy";
   }
 
   return (
-    <main>
-      <h1>TODO</h1>
-      <button>Sunny Time</button>
-      <button>Rain Time</button>
+    <main className={getBackgroundColor()}>
+      <h1>{getTitle()}</h1>
+      <button onClick={onSunClick}>Sunny Time</button>
+      <button onClick={onRainClick}>Rain Time</button>
     </main>
   );
 }
--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
-  /* You will need to  use  state to mnage the current image */
+  /* You will need to  use  state to manage the current image */
 
   /* You will need to hanle the click on left and right button */
 
